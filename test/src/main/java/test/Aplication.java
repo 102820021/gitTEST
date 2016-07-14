f08d28d8b2e0d83a7e4d0d289e9961d9a087e79b
102820021 <102820021wsx987@gmail.com>
2016-07-14 11:07:19 +0800

    test4

--- test/src/main/java/test/Aplication.java
+++ test/src/main/java/test/Aplication.java
@@ -1,5 +1,11 @@
 package test;
 
+import java.io.Console;
+
 public class Aplication {
 	int test2;
+	public static void main(String[] args) {
+        System.out.print("請輸入名稱：");
+        System.out.println("您輸入的名稱…");
+    }
 }
